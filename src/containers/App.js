be9e95879a44047b2139d4768a9129b8be5bc00d
Anh Tuan Tran <abc@example.com>
2021-01-20 14:31:54 +0100

    deploy v6

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./app-styles.css"
 
 // Importing React Router
-import {Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
@@ -18,17 +18,18 @@ import Geschichte from "./weingut/Geschichte";
 function App() {
   return (
     <div>
-
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
       
-      <Route path="/home" component={Aktuelles} />
+      <Route exact path="/" component={Aktuelles} />
       {/* <Route path="/home" component={Aktuelles} /> */}
       <Route path="/weingut" exact component={Weingut_main} />
       
 
       </Switch>
       <Footer />
+      </HashRouter>
     </div>
   );
 }
