51357e1f1e4e5449bc98b1a85e9cfdcc10e330ca
Anh Tuan Tran <abc@example.com>
2021-01-24 16:32:20 +0100

    add envelope in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,9 +2,14 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
-  faFacebookSquare,
-  faInstagramSquare
+  // faFacebookSquare,
+  // faFacebookF,
+  faFacebook,
+  // faInstagramSquare,
+  faInstagram
 } from '@fortawesome/free-brands-svg-icons';
+
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import "./footer.css"
 
@@ -16,13 +21,13 @@ const Footer = () => {
     <footer>
       <div>
         <p>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Loreley-Kellerei-244489822309556"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faFacebookSquare} /></em></a>
-          <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/loreleykellerei?igshid=p3k6kyseb2vt"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faInstagramSquare} /></em></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Loreley-Kellerei-244489822309556"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faFacebook} /></em></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/loreleykellerei?igshid=p3k6kyseb2vt"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faInstagram} /></em></a>
+          <a target="_blank" rel="noopener noreferrer" href="mailto:Loreley-kellerei@t-online.de"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faEnvelope} /></em></a>
         </p>
         <strong className="footerelements"> Impressum</strong>
         <strong className="footerelements"> Datenschutz</strong>
         <strong className="footerelements"> AGB</strong>
-        <strong className="footerelements"> Kontakt</strong>
         <strong className="footerelements"> Agrarumwelt- und Klimamaßnahmen</strong>
         <p>
           Loreley-Kellerei Leonhard; Nastätter Str. 9, 56346 Sankt Goarshausen; Tel. 06771 2659; E-mail: Loreley-kellerei@t-online.de
