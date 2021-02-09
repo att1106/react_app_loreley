782b1643702e011928bae68f22b3b6959c61711a
Anh Tuan Tran <abc@example.com>
2021-02-09 23:13:32 +0100

    added addresses in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
@@ -10,12 +10,14 @@ import {
   faInstagram
 } from '@fortawesome/free-brands-svg-icons';
 
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 import "./footer.css"
 
 
 const year = new Date().getFullYear();
+const weingutAddress = "https://www.google.de/maps/place/Loreley-Kellerei+Leonhard+Weingut/@50.157667,7.7141598,17z/data=!4m5!3m4!1s0x47be75ec6c57e33d:0xe1913f9139b37283!8m2!3d50.157667!4d7.7163538";
+const vinothekAddress = "https://www.google.de/maps/place/Vinothek+Loreley+an+der+F%C3%A4hre+St.+Goarshausen/@50.1537162,7.7159134,17z/data=!3m1!4b1!4m5!3m4!1s0x47be7584c2e2061d:0x6ff4234863f23de6!8m2!3d50.1537162!4d7.7181074";
 
 const Footer = () => {
   return (
@@ -26,6 +28,15 @@ const Footer = () => {
           <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/loreleykellerei?igshid=p3k6kyseb2vt"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faInstagram} /></em></a>
           <a target="_blank" rel="noopener noreferrer" href="mailto:Loreley-kellerei@t-online.de"><em className="socialicon fa-lg"><FontAwesomeIcon icon={faEnvelope} /></em></a>
         </p>
+        <div className="footer-address">
+          <p>Wir freuen uns auf Ihren Besuch </p>
+          <a target="_blank" rel="noopener noreferrer" href={weingutAddress}>
+            in unserem Weingut<em className="socialicon fa-lg"><FontAwesomeIcon icon={faMapMarkerAlt} /></em>
+          </a> 
+          <a target="_blank" rel="noopener noreferrer" href={vinothekAddress}>
+            in unserer Vinothek<em className="socialicon fa-lg"><FontAwesomeIcon icon={faMapMarkerAlt} /></em>
+          </a>
+        </div>
 
         <Link to="/impressum" className="footerelements">
           <strong>Impressum</strong>
