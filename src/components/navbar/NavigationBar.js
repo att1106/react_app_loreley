4ee412f7f43ae43e7e36116da56d25676ebdf9c0
Anh Tuan <at.tran1992@gmail.com>
2021-02-28 13:29:31 +0100

    navbar responsive

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import { Link } from "react-router-dom";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavbarBrand, NavDropdown } from "react-bootstrap";
 import { HashLink as Link } from "react-router-hash-link";
 import ImageImport from "../structure/ImageImport";
 
@@ -13,18 +13,35 @@ const NavigationBar = () => {
   const [menuOpen2, setMenuOpen2] = useState(false);
   const [menuOpen3, setMenuOpen3] = useState(false);
 
+
   return (
 
     <Navbar fixed="top" bg="white" expand="lg">
+      {/* toggler */}
+      <Navbar.Toggle 
+        className="navbar-toggler" 
+        bg="white" 
+        aria-controls="basic-navbar-nav"
+        />
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav
-          className="headnavbar justify-content-center"
-          style={{ flex: 1 }}
-        >
-        
-         <NavDropdown
+      {/* logo for mobile and tablet screes */}
+      <div className="logo-mobile">
+        <Nav.Link className="navbarlink navelement logo" href={process.env.PUBLIC_URL}>
+          <ImageImport
+            img={process.env.PUBLIC_URL + "/img/Logo_White.jpg"}
+            className="nav-logo"
+            alt="Logo"
+            width={60}
+          />
+        </Nav.Link>
+      </div>
+
+      {/* collapsing elements */}
+      <Navbar.Collapse>
+        <Nav className="headnavbar justify-content-center">
+
+          {/* Info */}
+          <NavDropdown
             className="navbarlink navelement" title="Info" id="basic-nav-dropdown"
             onMouseEnter={() => {
               setMenuOpen(true);
@@ -33,13 +50,17 @@ const NavigationBar = () => {
               setMenuOpen(false);
             }}
             show={menuOpen}
-           >
-            <Link to="/home#aktuelles" className="nav-link navbarlink">Aktuelles</Link>
-            <Link to="/home#termine" className="nav-link navbarlink">Termine</Link>
-            <Link to="/home#oeffnungszeiten" className="nav-link navbarlink">Öffnungszeiten</Link>
+            onClick={()=> setMenuOpen(m => !m)}
+          >
+            <Link to="/home#aktuelles" className="nav-link navbarlink" 
+            onClick={()=> {
+              setMenuOpen(false)
+            }}> Aktuelles</Link>
+            <Link to="/home#termine" className="nav-link navbarlink" onClick={()=> setMenuOpen(false)}>Termine</Link>
+            <Link to="/home#oeffnungszeiten" className="nav-link navbarlink" onClick={()=> setMenuOpen(false)}>Öffnungszeiten</Link>
           </NavDropdown>
-          
 
+          {/* Weingut */}
           <NavDropdown
             className="navbarlink navelement"
             title="Weingut"
@@ -51,26 +72,29 @@ const NavigationBar = () => {
               setMenuOpen2(false);
             }}
             show={menuOpen2}
-            >
-            <Link to="/weingut#allgemein" className="nav-link navbarlink">Weingut</Link>
-            <Link to="/weingut#vinothek" className="nav-link navbarlink">Vinothek</Link>
-            <Link to="/weingut#geschichte" className="nav-link navbarlink">Geschichte</Link>
-            <Link to="/weingut#weinberge" className="nav-link navbarlink">Weinberge</Link>
-            <Link to="/weingut#keller" className="nav-link navbarlink">Keller</Link>
+            onClick={()=> setMenuOpen2(m => !m)}
+          >
+            <Link to="/weingut#allgemein" className="nav-link navbarlink" onClick={()=> setMenuOpen2(false)} >Weingut</Link>
+            <Link to="/weingut#vinothek" className="nav-link navbarlink" onClick={()=> setMenuOpen2(false)}>Vinothek</Link>
+            <Link to="/weingut#geschichte" className="nav-link navbarlink" onClick={()=> setMenuOpen2(false)}>Geschichte</Link>
+            <Link to="/weingut#weinberge" className="nav-link navbarlink" onClick={()=> setMenuOpen2(false)}>Weinberge</Link>
+            <Link to="/weingut#keller" className="nav-link navbarlink" onClick={()=> setMenuOpen2(false)}>Keller</Link>
           </NavDropdown>
 
-          <Nav.Link className="navbarlink navelement logo" href={process.env.PUBLIC_URL}>
-            <ImageImport
-              img={process.env.PUBLIC_URL + "/img/Logo_White.jpg"}
-              className="nav-logo"
-              alt="Logo"
-              width={75}
-            />
-          </Nav.Link>
+          <div className="logo-desktop">
+            <Nav.Link className="navbarlink navelement logo" href={process.env.PUBLIC_URL}>
+              <ImageImport
+                img={process.env.PUBLIC_URL + "/img/Logo_White.jpg"}
+                className="nav-logo"
+                alt="Logo"
+                width={75}
+              />
+            </Nav.Link></div>
 
 
+          {/* Weine */}
           <NavDropdown
-            className="navbarlink navelement" title="Weine" id="basic-nav-dropdown"
+            className="navbarlink navelement weine" title="Weine" id="basic-nav-dropdown"
             onMouseEnter={() => {
               setMenuOpen3(true);
             }}
@@ -78,20 +102,17 @@ const NavigationBar = () => {
               setMenuOpen3(false);
             }}
             show={menuOpen3}
-            >
-            <Link to="/weine" className="nav-link navbarlink weine">Qualitätspyramide</Link>
-
+            onClick={()=> setMenuOpen3(false)}
+            onClick={()=> setMenuOpen3(m => !m)}
+          >
+            <Link to="/weine" className="nav-link navbarlink weine" onClick={()=> setMenuOpen3(false)}>Qualitätspyramide</Link>
           </NavDropdown>
 
-
-          <Nav.Link className="navbarlink navelement" target="_blank" rel="noopener noreferrer" href="https://loreley-kellerei.winitas-shop.de/">Shop</Nav.Link>
-
-
+          {/* Shop */}
+          <Nav.Link className="navbarlink navelement shop" target="_blank" rel="noopener noreferrer" href="https://loreley-kellerei.winitas-shop.de/">Shop</Nav.Link>
         </Nav>
-
-
-
       </Navbar.Collapse>
+
     </Navbar>
 
 
